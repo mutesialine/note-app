e3eb0b442316fd878e2c3627bbb90a6c1be16794
Mutesi <ktesialine@gmail.com>
2023-03-06 16:22:56 +0200

    feat:note app delete note functionality

--- src/App.js
+++ src/App.js
@@ -1,54 +1,61 @@
 import React from "react";
 import { useState } from "react";
 import Sidebar from "./components/Sidebar";
-import { data } from "./data";
 import Editor from "./components/Editor";
 import Split from "react-split";
 import { nanoid } from "nanoid";
-export default function App(){
-  const[notes, setNotes]= useState( () => JSON.parse(localStorage.getItem("notes")) || []
-    )
+export default function App() {
+  const [notes, setNotes] = useState(
+    () => JSON.parse(localStorage.getItem("notes")) || []
+  );
 
   const [currentNoteId, setCurrentNoteId] = React.useState(
-        (notes[0] && notes[0].id) || ""
-      );
+    (notes[0] && notes[0].id) || ""
+  );
 
-      React.useEffect(() => {
-        localStorage.setItem("notes", JSON.stringify(notes));
-      }, [notes]);
+  React.useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
 
-
-       function updateNote(text) {
-         setNotes((oldNotes) =>
-           oldNotes.map((oldNote) => {
-             return oldNote.id === currentNoteId
-               ? { ...oldNote, body: text }
-               : oldNote;
-           })
-         );
-       }
- 
-        function findCurrentNote() {
-          return (
-            notes.find((note) => {
-              return note.id === currentNoteId;
-            }) || notes[0]
-          );
+  function updateNote(text) {
+    setNotes((oldNotes) => {
+      const newArray = [];
+      for (let i = 0; i < oldNotes.length; i++) {
+        const oldNote = oldNotes[i];
+        if (oldNote.id === currentNoteId) {
+          newArray.unshift({ ...oldNote, body: text });
+        } else {
+          newArray.push(oldNote);
         }
-    
+      }
+      return newArray;
+    });
+  }
 
+  function deleteNote(event, noteId) {
+    event.stopPropagation();
+    setNotes((oldNotes) => oldNotes.filter((note) => note.id !== noteId));
+  }
 
-       function createNewNote() {
-         const newNote = {
-           id: nanoid(),
-           body: "# Type your markdown note's title here",
-         };
-         setNotes((prevNotes) => [newNote, ...prevNotes]);
-         setCurrentNoteId(newNote.id);
-       }
-    
+  function findCurrentNote() {
+    return (
+      notes.find((note) => {
+        return note.id === currentNoteId;
+      }) || notes[0]
+    );
+  }
+
+  function createNewNote() {
+    const newNote = {
+      id: nanoid(),
+      body: "# Type your markdown note's title here",
+    };
+    setNotes((prevNotes) => [newNote, ...prevNotes]);
+    setCurrentNoteId(newNote.id);
+  }
+
   return (
-    <main className="">
+    <main className="w-full max-w-7xl mx-auto">
       {notes.length > 0 ? (
         <Split sizes={[30, 70]} direction="horizontal" className="flex">
           <Sidebar
@@ -56,6 +63,7 @@ export default function App(){
             currentNote={findCurrentNote()}
             setCurrentNoteId={setCurrentNoteId}
             newNote={createNewNote}
+            deleteNote={deleteNote}
           />
 
           {currentNoteId && notes.length > 0 && (
@@ -75,5 +83,4 @@ export default function App(){
       )}
     </main>
   );
-
 }
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,26 +1,34 @@
 import React from "react";
 export default function Sidebar(props) {
-   const noteElements = props.notes.map((note, index) => (
-     <div className="" key={note.id}>
-       <div
-         className={`space-y-6  hover:text-white hover:bg-[#4A4E74]   ${
-           note.id === props.currentNote.id ? "selected-note" : ""
-         }`}
-         onClick={() => props.setCurrentNoteId(note.id)}
-       >
-         <h4 className="px-7 py-5 cursor-pointer text-2xl w-fit">
-           Note {index + 1}
-         </h4>
-       </div>
-     </div>
-   ));
+  const noteElements = props.notes.map((note, index) => (
+    <div key={note.id}>
+      <div
+        className={`flex items-center justify-content-center  gap-4 space-y-6  hover:text-white hover:bg-[#4A4E74] group   ${
+          note.id === props.currentNote.id ? "selected-note" : ""
+        }`}
+        onClick={() => props.setCurrentNoteId(note.id)}
+      >
+        <h4 className="px-7 py-5 cursor-pointer text-2xl w-fit">
+          {note.body.split("\n")[0]}
+        </h4>
+
+        <button onClick={(event) => props.deleteNote(event, note.id)}>
+          <img
+            src="/icon/delete.svg"
+            alt="delete"
+            className="invert-0 w-fit hidden group-hover:block"
+          />
+        </button>
+      </div>
+    </div>
+  ));
 
   return (
-    <div className=" shadow-md w-[20%]   ">
+    <div className=" shadow-md w-[20%] mb-16   ">
       <div className="flex px-7  items-center gap-6 py-8">
         <p className="text-4xl font-bold">Note</p>
         <button
-          className="text-4xl px-5 py-2 text-white bg-[#4A4E74]"
+          className="text-4xl px-5 rounded-md py-2 text-white bg-[#4A4E74]"
           onClick={props.newNote}
         >
           +
@@ -28,29 +36,5 @@ export default function Sidebar(props) {
       </div>
       {noteElements}
     </div>
-
-    // <div className="w-full">
-    //   <div className="max-w-[600px] mx-auto shadow-md  px-8 mt-24">
-    //     <div className=" flex items-center gap-6 py-8">
-    //       <p className="text-4xl">Note</p>
-    //       <p className="text-4xl px-2 py-1 bg-violet-700" onClick={getNumb}>
-    //         +
-    //       </p>
-    //     </div>
-    //     <div className="space-y-6">
-    //       {numb.map((value, key) => {
-    //         return (
-    //           <p className="hover:px-4 hover:bg-violet-700 w-fit">Note{key}</p>
-    //         );
-    //       })}
-    //     </div>
-    //   </div>
-    // </div>
-
-    // const [numb, setNumb] = React.useState([1]);
-
-    // function getNumb() {
-    //   setNumb((prev) => [...prev, prev + 1]);
-    // }
   );
 }
